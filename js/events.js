a2af4cca36d2b4205a2ef1b5dc6d6796d6a2e8b2
leo372239-1024 <leo372239@gmail.com>
2026-09-10 21:35:46 +0800

    sync: delete: t26 1

--- js/events.js
+++ js/events.js
@@ -115,20 +115,6 @@
       sampleUrl: "",
       owners: owners([])
     },
-    b7: {
-      short: "交大人节”文艺晚会",
-      who: "",
-      when: "已报名的同学请于18：35前就坐。",
-      where: "主校区西操场",
-      files: "凭票与一卡通入场，票号将作为现场抽奖凭证。",
-      steps: [],
-      stepImg: "",
-      attachments: [],
-      tips: "",
-      tipsImg: "",
-      sampleUrl: "",
-      owners: owners([])
-    },
     _roles: ROLES
   };
 });
